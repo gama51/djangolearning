ce36928958a1c4d00e3c677c90148ec2267d135c
Gabriel <gama51dr@gmail.com>
2021-12-11 18:44:48 -0500

    feat: se interactua con las opciones del admin

--- platzigram/users/models.py
+++ platzigram/users/models.py
@@ -7,10 +7,9 @@ from django.utils import tree
 class Profile(models.Model):
     user=models.OneToOneField(User, on_delete=models.CASCADE)
     website=models.URLField(max_length=200, blank=True)
-    biography=models.TextField(blank=True),
     phone_number=models.CharField(max_length=20,blank=True)
     picture=models.ImageField(upload_to='users/picture',blank=True,null=True)
-
+    biography=models.TextField(blank=True,null=True)
     created=models.DateTimeField(auto_now_add=True)
     modified=models.DateTimeField(auto_now=True)
 
