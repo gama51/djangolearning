b8afd940cfe705ba5df5a955fbe9385969690678
Gabriel <gama51dr@gmail.com>
2021-12-11 16:46:01 -0500

    feat: se hacen diferentes modelos, y se utiliza el modelo de usuario de django así como se inícia el trabajo con el admin

--- platzigram/posts/models.py
+++ platzigram/posts/models.py
@@ -9,6 +9,10 @@ class user(models.Model):
     email=models.EmailField(unique=True)
     password=models.CharField(max_length=100)
 
+    country=models.CharField(max_length=50,blank=True,null=True)
+    city=models.CharField(max_length=50,blank=True,null=True)
+    is_admin=models.BooleanField(default=False)
+
     first_name=models.CharField(max_length=100)
     last_name=models.CharField(max_length=100)
 
@@ -17,5 +21,8 @@ class user(models.Model):
 
     created=models.DateTimeField(auto_now_add=True)
     modified=models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return str(self.email+':'+self.city+':'+self.country)
     
 
